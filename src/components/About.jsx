1a3e25915d22014b23ac1235109bac4df082d626
vedant-agarwal-08 <105448072+vedant-agarwal-08@users.noreply.github.com>
2023-05-14 02:08:07 +0530

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+        <p className='text-xl mt-20'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, quaerat? Sapiente vero eligendi dolor accusamus laudantium. Totam itaque unde harum officiis eveniet sequi animi excepturi minus sunt perferendis. Sed nulla ipsa culpa voluptatum voluptatem accusantium, odio atque! Aliquam architecto non, corrupti dolor voluptatum neque, dolores itaque veritatis suscipit iste repudiandae?</p>
+
+        <br />
+        
+        <p className='text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ullam atque officiis reprehenderit commodi nihil repellat dicta quasi quidem blanditiis odio, totam et id modi sit labore rem eos minima quo saepe assumenda consectetur? Quod quam culpa id ipsam, sequi, cumque nisi earum suscipit inventore numquam explicabo, voluptas similique corrupti.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
